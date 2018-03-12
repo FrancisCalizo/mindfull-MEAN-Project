9c7fbbcbd5a333ad5b719c59760c2b3fefee321c
Francis Calizo <francisacalizo@gmail.com>
2018-03-12 15:52:40 -0400

    Auth Logic completed in app.js

--- server-mindfull/app.js
+++ server-mindfull/app.js
@@ -1,14 +1,23 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express       = require('express');
+const path          = require('path');
+const favicon       = require('serve-favicon');
+const logger        = require('morgan');
+const cookieParser  = require('cookie-parser');
+const bodyParser    = require('body-parser');
+// Additional imports
+const mongoose      = require("mongoose");
+const session       = require('express-session');
+const passport      = require('passport');
+const cors          = require('cors');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
 
-var app = express();
+// Connect Mongoose to the DB
+mongoose.connect('mongodb://localhost/mindfull');
+
+// Passport Setup
+require('./configs/passport-config');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,25 +31,53 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Add Express-Session Logic
+app.use(session({
+  secret:"This is a secret, please be MINDFUL!",
+  resave: true,
+  saveUninitialized: true
+}));
+
+// Add Passport Logic
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(
+  cors({
+    credentials: true,                 // allow other domains to send cookies
+    origin: ["http://localhost:4200"]  // these are the domains that are allowed
+  })
+);
+
+// ======== Routes =========== //
+const index = require('./routes/index');
 app.use('/', index);
-app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+const authRoutes = require("./routes/auth-routes");
+app.use("/", authRoutes);
+
+// =========================== //
+
+app.use((req, res, next) => {
+  // If no routes match, send them the Angular HTML.
+  res.sendFile(__dirname + "/public/index.html");
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 module.exports = app;
