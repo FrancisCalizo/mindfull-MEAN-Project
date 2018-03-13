3da2e854b1079ad06584a29f42b04bb3effc195c
Francis Calizo <francisacalizo@gmail.com>
2018-03-13 13:51:20 -0400

    Dashboard only implements importing a Morning OR Evening Entry, not both

--- server-mindfull/routes/dashboard-routes.js
+++ server-mindfull/routes/dashboard-routes.js
@@ -6,6 +6,41 @@ const EveningFull   = require('../models/eveningfull');
 
 const dashboardRoute  = express.Router();
 
+dashboardRoute.get('/dashboard', (req, res, next) => {
+  if (!req.user) {
+      res.status(401).json({ errorMessage: 'Not logged in.' });
+      return;
+  }
 
+  MorningFull.find(
+      { user: req.user._id }
+  )
+  // .limit(7)
+  .sort({ date: 1 })
+  .exec((err, morningEntries) => {
+      if (err) {
+          console.log('Error finding entries', err);
+          res.status(500).json({ errorMessage: 'Finding entries went wrong'});
+          return;
+      }
+
+      res.status(200).json(morningEntries);
+  });
+
+  EveningFull.find(
+    { user: req.user._id }
+  )
+  // .limit(7)
+  .sort({ date: 1 })
+  .exec((err, eveningEntries) => {
+      if (err) {
+          console.log('Error finding entries', err);
+          res.status(500).json({ errorMessage: 'Finding entries went wrong'});
+          return;
+      }
+
+      res.status(200).json(eveningEntries);
+  });
+});
 
 module.exports = dashboardRoute;
